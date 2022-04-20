091f4eca09378217d91a76f3a78acc356d4a575b
Sam Clark <samuel.clark84@yahoo.com>
2022-04-20 16:39:30 -0500

    api curl

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,9 @@
 var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+    fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+        console.log("inside", response);
+    });
+
+    console.log("outside");
 };
 
 getUserRepos();
